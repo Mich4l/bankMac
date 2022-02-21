26311a04a98351bb910ad2c519fd3ce0470db236
Mich4l <mich4l12@wp.pl>
2022-02-21 08:30:40 +0100

    Initial commit

---
+++ src/main/java/com/macbank/bankMac/BankMacApplication.java
@@ -0,0 +1,13 @@
+package com.macbank.bankMac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macbank/bankMac/BankMacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macbank.bankMac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
